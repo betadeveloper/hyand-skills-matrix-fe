4c51f42ee6ac55d47634b8a4d60db635a629767e
Raigardas <raigardas.tautkus@knf.stud.vu.lt>
2024-09-24 22:38:26 +0300

    Added add skills button and logic for non existing skills

--- src/pages/career/Career.tsx
+++ src/pages/career/Career.tsx
@@ -129,90 +129,109 @@ const Career = () => {
   };
 
   return (
-    <>
-      <Box sx={{ width: '100%', mb: 0.5 }}>
-        <Typography sx={{ fontSize: 32, fontWeight: 'bold', textAlign: 'center' }}>
-          Score: {score.toFixed(2)}
-        </Typography>
-        <LinearProgress variant="determinate" value={(score / 360) * 100} sx={{ height: '24px' }} />
-      </Box>
-      <Box display={'flex'}>
-        {careerLevels.map((careerLevelEl: CareerLevel, index: number) => (
-          <Box key={index} flex={1}>
-            <Card
-              variant="outlined"
-              sx={{
-                backgroundColor: careerLevelEl === careerLevel ? 'primary.main' : 'white',
-                color: careerLevelEl === careerLevel ? 'white' : 'black'
-              }}>
-              <CardContent>
-                <Box display="flex" alignItems="center">
-                  <Typography variant="h5" fontWeight={'Bold'}>
+      <>
+        <Box sx={{ width: '100%', mb: 0.5 }}>
+          <Typography sx={{ fontSize: 32, fontWeight: 'bold', textAlign: 'center' }}>
+            Score: {score.toFixed(2)}
+          </Typography>
+          <LinearProgress variant="determinate" value={(score / 360) * 100} sx={{ height: '24px' }} />
+        </Box>
+        <Box display={'flex'}>
+          {careerLevels.map((careerLevelEl: CareerLevel, index: number) => (
+              <Box key={index} flex={1}>
+                <Card
+                    variant="outlined"
+                    sx={{
+                      backgroundColor: careerLevelEl === careerLevel ? 'primary.main' : 'white',
+                      color: careerLevelEl === careerLevel ? 'white' : 'black'
+                    }}>
+                  <CardContent>
+                    <Box display="flex" alignItems="center">
+                      <Typography variant="h5" fontWeight={'Bold'}>
                     <span style={{ visibility: careerLevelEl === careerLevel ? 'visible' : 'hidden' }}>
                       Current career level:
                     </span>
-                    <br />
-                    {careerLevelEl}
-                  </Typography>
-                </Box>
-              </CardContent>
-            </Card>
-          </Box>
-        ))}
-      </Box>
+                        <br />
+                        {careerLevelEl}
+                      </Typography>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Box>
+          ))}
+        </Box>
 
       {careerPaths && (
-        <Box display={'flex'} alignItems={'center'} justifyContent={'center'} flexDirection={'column'}>
-          <Typography variant="h6" mt={3}>
-            Skills for <b>{careerPaths[0].name}</b> Career Path
-          </Typography>
-          {careerPaths[0].skills.map((skill, index) => (
-            <Box key={index} sx={{ marginBottom: 2 }}>
-              <Typography variant="body1">{skill.name}</Typography>
-              <Select
-                sx={{ width: '400px' }}
-                defaultValue={0}
-                onChange={(event) => {
-                  const newProficiencies = [...selectedProficiencies];
-                  newProficiencies[index] = Number(event.target.value);
-                  setSelectedProficiencies(newProficiencies);
-                }}>
-                <MenuItem value={0} disabled>
-                  Select proficiency
-                </MenuItem>
-                <MenuItem value={1} title="Beginner - Fundamentals, basic abilities with the skill">
-                  Beginner
-                </MenuItem>
-                <MenuItem value={2} title="Medium - Have some experience with this skill">
-                  Medium
-                </MenuItem>
-                <MenuItem value={3} title="Advanced - Have substantial understanding and experience with this skill">
-                  Advanced
-                </MenuItem>
-                <MenuItem value={4} title="Master - Have deep understanding and experience with this skill">
-                  Master
-                </MenuItem>
-                <MenuItem value={5} title="Expert - Have comprehensive and authoritative knowledge of this skill">
-                  Expert
-                </MenuItem>
-              </Select>
-            </Box>
-          ))}
-          <Box>
-            <Button
-              onClick={calculateCareerLevel}
-              sx={{
-                backgroundColor: 'primary.main',
-                color: 'white',
-                padding: '8px 16px',
-                borderRadius: '8px',
-                ':hover': { backgroundColor: 'primary.main', opacity: 0.8 }
-              }}>
-              Evaluate
-            </Button>
-            <Button sx={{ ml: 20 }} variant="outlined" color="primary" onClick={handleClickOpen}>
-              Book Meeting
-            </Button>
+          <Box display={'flex'} alignItems={'center'} justifyContent={'center'} flexDirection={'column'}>
+            <Typography variant="h6" mt={3}>
+              Skills for <b>{careerPaths[0].name}</b> Career Path
+            </Typography>
+            {careerPaths[0].skills.length > 0 ? (
+                careerPaths[0].skills.map((skill, index) => (
+                    <Box key={index} sx={{ marginBottom: 2 }}>
+                      <Typography variant="body1">{skill.name}</Typography>
+                      <Select
+                          sx={{ width: '400px' }}
+                          defaultValue={0}
+                          onChange={(event) => {
+                            const newProficiencies = [...selectedProficiencies];
+                            newProficiencies[index] = Number(event.target.value);
+                            setSelectedProficiencies(newProficiencies);
+                          }}>
+                        <MenuItem value={0} disabled>
+                          Select proficiency
+                        </MenuItem>
+                        <MenuItem value={1} title="Beginner - Fundamentals, basic abilities with the skill">
+                          Beginner
+                        </MenuItem>
+                        <MenuItem value={2} title="Medium - Have some experience with this skill">
+                          Medium
+                        </MenuItem>
+                        <MenuItem value={3} title="Advanced - Have substantial understanding and experience with this skill">
+                          Advanced
+                        </MenuItem>
+                        <MenuItem value={4} title="Master - Have deep understanding and experience with this skill">
+                          Master
+                        </MenuItem>
+                        <MenuItem value={5} title="Expert - Have comprehensive and authoritative knowledge of this skill">
+                          Expert
+                        </MenuItem>
+                      </Select>
+                    </Box>
+                ))
+            ) : (
+                <Typography>No skills available for this career path</Typography>
+            )}
+            {careerPaths[0].skills.length > 0 && (
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '400px', mt: 2 }}>
+                  <Button
+                      onClick={calculateCareerLevel}
+                      sx={{
+                        backgroundColor: 'primary.main',
+                        color: 'white',
+                        padding: '8px 16px',
+                        borderRadius: '8px',
+                        ':hover': { backgroundColor: 'primary.main', opacity: 0.8 }
+                      }}
+                  >
+                    Evaluate
+                  </Button>
+                  <Button sx={{ ml: 20 }} variant="outlined" color="primary" onClick={handleClickOpen}>
+                    Book Meeting
+                  </Button>
+                </Box>
+            )}
+            {careerPaths[0].skills.length === 0 && (
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => {
+                      // TODO: Add skills logic here
+                    }}
+                >
+                  Add skills
+                </Button>
+            )}
             <Dialog open={open} onClose={handleClose} fullWidth>
               <DialogTitle fontSize={32}>Book a Meeting With Owner</DialogTitle>
               <DialogContent>
@@ -224,18 +243,18 @@ const Career = () => {
                   Score: {`${score.toPrecision(4)}`}
                 </Typography>
                 <TextField
-                  autoFocus
-                  margin="dense"
-                  id="date"
-                  label="Meeting Date"
-                  type="datetime-local"
-                  fullWidth
-                  value={date}
-                  onChange={(e) => setDate(e.target.value)}
-                  InputLabelProps={{
-                    shrink: true
-                  }}
-                  required
+                    autoFocus
+                    margin="dense"
+                    id="date"
+                    label="Meeting Date"
+                    type="datetime-local"
+                    fullWidth
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
+                    InputLabelProps={{
+                      shrink: true
+                    }}
+                    required
                 />
               </DialogContent>
               <DialogActions>
@@ -245,9 +264,8 @@ const Career = () => {
             </Dialog>
             <ToastContainer />
           </Box>
-        </Box>
       )}
-    </>
+      </>
   );
 };
 
